fc79ae3a572bf825e56a9922850b6c04dfbdc197
LukasPokryvka <58267318+LukasPokryvka@users.noreply.github.com>
2023-01-06 15:35:33 +0100

    Bank preview Page

--- src/pages/bank/[...bankId].tsx
+++ src/pages/bank/[...bankId].tsx
@@ -1,8 +1,24 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import ClientForm from "../../components/forms/ClientForm";
 import withAuth from "../../components/withAuth";
 
 const BankPreview: NextPage = () => {
-  return <div>Bank Preview</div>;
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const bankId = router.query["bankId"] as string;
+
+  return (
+    <section className="flex justify-center">
+      <div className="card bg-neutral shadow-xl w-1/3">
+        <div className="card-body">
+          <h2 className="card-title">Bank</h2>
+          <ClientForm clientId={bankId} queryClient={queryClient} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default withAuth(BankPreview);
