0c3a874904ae835bb07c759747ac96a4e01f5960
LukasPokryvka <58267318+LukasPokryvka@users.noreply.github.com>
2023-01-06 15:43:52 +0100

    Update Sidebar to include bank

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
+import { createBank, useBanks } from "../../../hooks/bankQueries";
 import { createClient, useClients } from "../../../hooks/clientQueries";
 import { createInvoice, useInvoices } from "../../../hooks/invoiceQueries";
 
@@ -35,50 +36,91 @@ const Sidebar: React.FunctionComponent = () => {
     },
   });
 
+  const { mutate: newBank } = useMutation(createBank, {
+    onSuccess: (data) => {
+      if (data) {
+        router.push(`/bank/${data.id}`);
+      }
+      toast.success("Bank record created successfuly");
+      queryClient.invalidateQueries({ queryKey: ["banks"] });
+    },
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
   const { data: invoices, error, isLoading } = useInvoices();
   const { data: clients } = useClients();
+  const { data: banks } = useBanks();
+
+  const renderSidebarButton = () => {
+    if (pathname.includes("/clients")) {
+      return (
+        <button onClick={() => newClient()} className="btn btn-primary w-full">
+          New client
+        </button>
+      );
+    }
+
+    if (pathname.includes("/bank")) {
+      return (
+        <button onClick={() => newBank()} className="btn btn-primary w-full">
+          New bank record
+        </button>
+      );
+    }
+
+    return (
+      <button onClick={() => newInvoice()} className="btn btn-primary w-full">
+        New invoice
+      </button>
+    );
+  };
+
+  const renderSidebarContent = () => {
+    if (pathname.includes("/clients")) {
+      return clients?.map((client) => (
+        <div key={client.id}>
+          <Link
+            className="btn btn-secondary btn-outline btn-wide"
+            href={`/clients/${client.id}`}
+          >
+            <div>{client.companyName}</div>
+          </Link>
+        </div>
+      ));
+    }
+
+    if (pathname.includes("/bank")) {
+      return banks?.map((bank) => (
+        <div key={bank.id}>
+          <Link
+            className="btn btn-secondary btn-outline btn-wide"
+            href={`/bank/${bank.id}`}
+          >
+            <div>{bank.bankName}</div>
+          </Link>
+        </div>
+      ));
+    }
+
+    return invoices?.map((invoice) => (
+      <div key={invoice.id}>
+        <Link
+          className="btn btn-secondary btn-outline btn-wide"
+          href={`/invoices/${invoice.id}`}
+        >
+          <div>{invoice.id}</div>
+        </Link>
+      </div>
+    ));
+  };
 
   return (
     <aside className="h-screen bg-base-200 w-72 border-r-2 border-secondary-focus border-dashed p-4">
-      <section className="flex justify-center">
-        {pathname.includes("/clients") ? (
-          <button
-            onClick={() => newClient()}
-            className="btn btn-primary w-full"
-          >
-            New client
-          </button>
-        ) : (
-          <button
-            onClick={() => newInvoice()}
-            className="btn btn-primary w-full"
-          >
-            New invoice
-          </button>
-        )}
-      </section>
+      <section className="flex justify-center">{renderSidebarButton()}</section>
       <section className="flex flex-col gap-2 mt-4">
-        {pathname.includes("/clients")
-          ? clients?.map((client) => (
-              <div key={client.id}>
-                <Link
-                  className="btn btn-secondary btn-outline btn-wide"
-                  href={`/clients/${client.id}`}
-                >
-                  <div>{client.companyName}</div>
-                </Link>
-              </div>
-            ))
-          : invoices?.map((invoice) => (
-              <div key={invoice.id}>
-                <Link
-                  className="btn btn-secondary btn-outline btn-wide"
-                  href={`/invoices/${invoice.id}`}
-                >
-                  <div>{invoice.id}</div>
-                </Link>
-              </div>
-            ))}
+        {renderSidebarContent()}
       </section>
     </aside>
   );
